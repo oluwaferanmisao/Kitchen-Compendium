9d3fbbed220c3273748b9071d6905edb2304727c
Oluwaferanmi Sao <caleb4sao@gmail.com>
2024-12-27 20:40:46 +0100

    First Commit

---
+++ android/app/src/main/kotlin/com/example/kitchen_compendium/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kitchen_compendium
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/manager/page_manager.dart
@@ -0,0 +1,14 @@
+import "package:flutter/material.dart";
+
+class PageManager extends StatelessWidget {
+  const PageManager({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Hello\nKitchen.'),
+      ),
+    );
+  }
+}
