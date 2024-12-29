639d96286758472cced632757c54113737bc6ed2
Oluwaferanmi Sao <caleb4sao@gmail.com>
2024-12-29 15:56:40 +0100

    Added theming and modified UI design

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,23 +10,42 @@ class HomePage extends StatelessWidget {
     return ListView.builder(
       itemCount: recipeList.length,
       itemBuilder: (context, index) {
-        return ListTile(
-          leading: imageSrc(index),
-          title: Text(
-            recipeList[index].name,
-            style: const TextStyle(
-              fontSize: 16.0,
-              fontWeight: FontWeight.bold,
+        return Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16.0,
+            vertical: 8.0,
+          ),
+          child: Card(
+            elevation: 1,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(4.0),
+              ),
+            ),
+            child: ListTile(
+              contentPadding: const EdgeInsets.symmetric(horizontal: 16.0),
+              leading: imageSrc(index),
+              title: Text(
+                recipeList[index].name,
+                style: const TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              subtitle: const Text(
+                "Tap for more details",
+                style: TextStyle(
+                  fontSize: 12.0,
+                  color: Colors.grey,
+                ),
+              ),
+              trailing: const Icon(
+                Icons.arrow_forward_ios,
+                size: 16.0,
+                color: Colors.grey,
+              ),
             ),
           ),
-          subtitle: const Text(
-            "Tap for more details",
-            style: TextStyle(
-              fontSize: 12.0,
-              color: Colors.grey,
-            ),
-          ),
-          style: ListTileStyle.drawer,
         );
       },
     );
